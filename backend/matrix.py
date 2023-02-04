1e916488c47de4d7a8036e108e6916c0a8db473d
Niall Oswald <niall.oswald@hotmail.co.uk>
2023-02-04 20:09:12 +0000

    Add creation to Similarity

--- backend/matrix.py
+++ backend/matrix.py
@@ -8,17 +8,20 @@ class Similarity:
 
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, matrix=None):
         if cls._instance is None:
             print("Loading similarity matrix...")
             cls._instance = super(Similarity, cls).__new__(cls)
-            cls._instance._load_matrix()
+            cls._instance._load_matrix(matrix)
 
         return cls._instance
 
-    def _load_matrix(self):
-        with open("../similarity_matrix.pkl", "rb") as f:
-            self.matrix = pickle.load(f)
+    def _load_matrix(self, matrix):
+        if matrix is None:
+            with open("../similarity_matrix.pkl", "rb") as f:
+                self.matrix = pickle.load(f)
+        else:
+            self.matrix = matrix
 
     def _save_matrix(self):
         with open("../similarity_matrix.pkl", "wb") as f:
