d63b3b1d9e13ac48ecd6934d6c7763bdd65fd53c
Charles Loveman <cel20@ic.ac.uk>
2023-02-04 18:39:29 +0000

    Feat: Link front and back end

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    print("hello")
+    return {"text": "Hello, World!"}
--- frontend/recommender/src/App.js
+++ frontend/recommender/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null)
+
+  function get() {
+    console.log("hello")
+    axios.get("http://127.0.0.1:5000/").then(function (response) {
+      console.log(response.data)
+      setData(response.data["text"]) 
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(data) ? 
+        <p>{data}</p>
+        :
+        <Button onClick={() => get()}>Get</Button>
+      }
     </div>
   );
 }
