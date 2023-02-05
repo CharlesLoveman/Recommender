17ff5660b997c4c26f531a236ba3545630bb8b3e
Niall Oswald <niall.oswald@hotmail.co.uk>
2023-02-05 00:32:10 +0000

    Add persistent mapping classes

--- backend/matrix.py
+++ backend/matrix.py
@@ -1,6 +1,8 @@
 """Module to store similarity matrix."""
 
 import pickle
+import numpy as np
+from functools import singledispatch
 
 
 class Similarity:
@@ -27,9 +29,14 @@ class Similarity:
         with open("../similarity_matrix.pkl", "wb") as f:
             pickle.dump(self.matrix, f)
 
+    def __getitem__(self, index):
+        return self.matrix[index]
+
 
 class Map:
-    """Class to store map from MAL ids to network indices."""
+    """Class to store map from network indices to MAL ids."""
+
+    _instance = None
 
     def __new__(cls, mapping=None):
         if cls._instance is None:
@@ -42,10 +49,53 @@ class Map:
     def _load_map(self, mapping):
         if mapping is None:
             with open("../id_mapping.pkl", "rb") as f:
-                self.mapping = pickle.load(f)
+                self.dictionary = pickle.load(f)
         else:
-            self.mapping = mapping
+            self.dictionary = mapping
+
+        def f(index):
+            return self.dictionary[index]
+
+        self.mapping = np.vectorize(f)
 
     def _save_map(self):
-        with open("../similarity_matrix.pkl", "wb") as f:
-            pickle.dump(self.mapping, f)
+        with open("../id_mapping.pkl", "wb") as f:
+            pickle.dump(self.dictionary, f)
+
+    def __call__(self, index):
+        if len(index):
+            return self.mapping(index)
+
+
+class InvMap:
+    """Class to store map from MAL ids to network indices."""
+
+    _instance = None
+
+    def __new__(cls, mapping=None):
+        if cls._instance is None:
+            print("Loading inverse id mapping...")
+            cls._instance = super(InvMap, cls).__new__(cls)
+            cls._instance._load_map(mapping)
+
+        return cls._instance
+
+    def _load_map(self, mapping):
+        if mapping is None:
+            with open("../inv_mapping.pkl", "rb") as f:
+                self.dictionary = pickle.load(f)
+        else:
+            self.dictionary = mapping
+
+        def f(id_):
+            return self.dictionary[id_]
+
+        self.mapping = np.vectorize(f)
+
+    def _save_map(self):
+        with open("../inv_mapping.pkl", "wb") as f:
+            pickle.dump(self.dictionary, f)
+
+    def __call__(self, id_):
+        if len(id_):
+            return self.mapping(id_)
