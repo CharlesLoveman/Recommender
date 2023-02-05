f7b9db03b79a4eca323e09b9dd626ae3914fce6e
Niall Oswald <niall.oswald@hotmail.co.uk>
2023-02-05 10:48:26 +0000

    Repair main

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request
 from flask_cors import CORS
+from mal_api import MAL_API
+
 
 app = Flask(__name__)
 CORS(app)
+MAL_API.pickle_global_entries()
 
 @app.route("/", methods=['GET'])
 def hello_world():
     members = request.args.getlist('members[]')
+    
+    for user in members:
+        user_entries = MAL_API.extract_user_entries(user)
+        
+
     print(members)
     return {"text": f"Hello, {members}!"}
+
--- backend/mal_api.py
+++ backend/mal_api.py
@@ -1,183 +1,93 @@
 import os
 import requests
-import secrets
-import json
 from time import sleep
-from pprint import pprint
 import numpy as np
 import pandas as pd
 import pickle
+from api import API
 
-class API:
+
+class MAL_API(API):
 
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
     headers = {
-            'X-MAL-CLIENT-ID': CLIENT_ID,
-            'Content-Type': "application/json"
-        }
+        'X-MAL-CLIENT-ID': CLIENT_ID,
+        'Content-Type': "application/json"
+    }
 
-    # 1. Generate a new Code Verifier / Code Challenge.
-    def get_new_code_verifier() -> str:
-        token = secrets.token_urlsafe(100)
-        return token[:128]
-
-
-    # 2. Print the URL needed to authorise your application.
-    def get_authorisation_code(code_challenge: str):
-
-        url = """https://myanimelist.net/v1/oauth2/authorize"""
-        data = {
-            'response_type': 'code',
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'code_challenge': code_challenge,
-        }
-        response = requests.get(url, data)
-        auth_code = response.json()
-        print(auth_code)
-        return 
-
-
-    # 3. Once you've authorised your application, you will be redirected to the webpage you've
-    #    specified in the API panel. The URL will contain a parameter named "code" (the Authorisation
-    #    Code). You need to feed that code to the application.
-    def generate_new_token(authorisation_code: str, code_verifier: str) -> dict:
-        global CLIENT_ID, CLIENT_SECRET
-
-        url = 'https://myanimelist.net/v1/oauth2/token'
-        data = {
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'code': authorisation_code,
-            'code_verifier': code_verifier,
-            'grant_type': 'authorization_code'
-        }
-
-        response = requests.post(url, data)
-        response.raise_for_status()  # Check whether the request contains errors
-
-        token = response.json()
-        response.close()
-        print('Token generated successfully!')
-
-        with open('token.json', 'w') as file:
-            json.dump(token, file, indent=4)
-            print('Token saved in "token.json"')
-
-        return token
-
-
-    # 4. Test the API by requesting your profile information
-    def print_user_info(access_token: str):
-        url = 'https://api.myanimelist.net/v2/users/@me'
-        response = requests.get(url, headers={
-            'Authorization': f'Bearer {access_token}'
-            })
-        response.raise_for_status()
-        user = response.json()
-        response.close()
-
-        print(f"\n>>> Greetings {user['name']}! <<<")
-
-
-    #code_verifier = code_challenge = get_new_code_verifier()
-    #authorisation_code = get_authorisation_code(code_challenge)
-    # print(authorisation_code)
-    # token = generate_new_token(authorisation_code, code_verifier)
-
-    # print_user_info(token['access_token'])
-
-    def pretty_print_POST(req):
-        """
-        At this point it is completely built and ready
-        to be fired; it is "prepared".
-
-        However pay attention at the formatting used in 
-        this function because it is programmed to be pretty 
-        printed and may differ from the actual request.
-        """
-        print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-            '-----------START-----------',
-            req.method + ' ' + req.url,
-            '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-            req.body,
-        ))
-
-    def get_user_opinion(list_status):
+    def get_user_opinion(self, list_status):
         if list_status['status'] in ("completed", "watching", "on_hold", "dropped"):
             return list_status['score']
         
     @classmethod
-    def extract_user_entries(user):
+    def extract_user_entries(cls, user):
         url = f"https://api.myanimelist.net/v2/users/{user}/animelist"
         data = {
-                "status": "completed",
-                "fields": "list_status",
-                "limit": 100
+            "status": "completed",
+            "fields": "list_status",
+            "limit": 100
         }
-        response = requests.get(url=url, params=data, headers=headers)
+        response = requests.get(url=url, params=data, headers=cls.headers)
         animes = response.json()
-        # Extract all users boys
-        # Handle none or 0 (if else)
-        print(animes)
+        
+        if 'error' in animes:
+            return None
+        else:
+            unfiltered_list = [[node['node']['id'], cls.get_user_opinion(node['list_status'])] for node in animes['data']]
+            unfiltered_list = list(filter(lambda a, b: b is not None and b != 0, unfiltered_list))
 
-        return np.array([[node['node']['id'], get_user_opinion(node['list_status'])] if  for node in animes['data']])
+            return np.array(unfiltered_list)
 
-
-
-    def get_anime_details(id):
+    def get_anime_details(cls, id):
         url = f"https://api.myanimelist.net/v2/anime/{id}"
 
         data = {
             'fields': "title,mean,recommendations"
         }
         
-        response = requests.get(url=url, params=data, headers=headers)
+        response = requests.get(url=url, params=data, headers=cls.headers)
         anime_details = response.json()
-
-        #pprint(anime_details)
 
         title = anime_details['title']
         rating = anime_details['mean']
         recommendations = np.array([node['node']['id'] for node in anime_details['recommendations']])
         return title, rating, recommendations
 
-    ret = extract_user_entries("anon1253")
-    print(ret)
+    @classmethod
+    def pickle_global_entries(cls):
+        if not os.path.exists("df.p"):
+            n = 20
+            anime_ids = []
 
-    def extract_global_entries():
-        n = 20
-        anime_ids = []
+            for i in range(n):
+                print(f"{i}/{n}")
+                url = "https://api.myanimelist.net/v2/anime/ranking"
 
-        for i in range(n):
-            print(f"{i}/{n}")
-            url = "https://api.myanimelist.net/v2/anime/ranking"
+                data = {
+                    'ranking_type': "all",
+                    'limit': 500,
+                    'offset': 500 * i,
+                    'fields': "recommendations,score"
+                }
+                sleep(1)
+                response = requests.get(url=url, params=data, headers=cls.headers)
 
-            data = {
-                'ranking_type': "all",
-                'limit': 500,
-                'offset': 500 * i,
-                'fields': "recommendations,score"
-            }
-            sleep(1)
-            response = requests.get(url=url, params=data, headers=headers)
+                animes = response.json()
+                for node in animes['data']:
+                    anime_ids.append(node['node']['id'])
 
-            animes = response.json()
-            for node in animes['data']:
-                anime_ids.append(node['node']['id'])
+            df = pd.DataFrame(columns=['id', 'name', 'rating', 'recommends'])
 
-        df = pd.DataFrame(columns=['id', 'name', 'rating', 'recommends'])
+            m = 0
+            print(f"Total: {len(anime_ids)}")
 
-        m = 0
-        print(f"Total: {len(anime_ids)}")
+            for id in anime_ids:
+                m += 1
+                print(m)
+                sleep(0.5)
+                title, rating, recommendations = cls.get_anime_details(id)
+                details_list = [id, title, rating, recommendations]
+                df.loc[len(df)] = details_list
 
-        for id in anime_ids:
-            m += 1
-            print(m)
-            sleep(0.5)
-            title, rating, recommendations = get_anime_details(id)
-            details_list = [id, title, rating, recommendations]
-            df.loc[len(df)] = details_list
-
-        pickle.dump(df, open("df.p", "wb"))
+            pickle.dump(df, open("df.p", "wb"))
