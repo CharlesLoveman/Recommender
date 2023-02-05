3dd42d89fdb4c40b6867e26bfaeb5774513ba67d
Akesh Mistry <akeshmistry15@gmail.com>
2023-02-05 10:51:12 +0000

    Uncomment recommend function

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,8 @@ MAL_API.pickle_global_entries()
 def hello_world():
     members = request.args.getlist("members[]")
 
-    # users_info = [MAL_API.extract_user_entries(user) for user in members]
-    # recommendation_id = recommend(users_info)
+    #users_info = [MAL_API.extract_user_entries(user) for user in members]
+    #recommendation_id = recommend(users_info)
     recommendation_id = 12189
 
     title, rating, image_url = MAL_API.get_anime_display_details(
