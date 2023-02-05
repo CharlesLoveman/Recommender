214d18ba58a2cb1656b33ae6c5eed257ba4a8803
Akesh Mistry <akeshmistry15@gmail.com>
2023-02-05 01:54:18 +0000

    Neaten API and MAL API

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
-class API:
 
-    @abstractmethod
+class API(ABC):
+    
     @classmethod
+    @abstractmethod
     def extract_user_entries(user):
         """Get an np array of tuples containing the id, and user score of each watched show."""
-        pass
+        return NotImplemented
+   
+    @abstractmethod
+    def extract_global_entries():
+        """Get a data frame of all the media entries, with headers 'id', 'title', 'rating' and 'recommends'. Also pickles the database."""
+        return NotImplemented
